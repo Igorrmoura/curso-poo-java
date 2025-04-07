d6ee63ea65225e63518e4defab45f664a24157a0
igor <igorkfz31@gmail.com>
2025-04-07 20:05:57 -0300

    add construtur product

--- ProgramTV/src/entities/Product.java
+++ ProgramTV/src/entities/Product.java
@@ -2,10 +2,18 @@ package entities;
 
 public class Product {
 	
+	// atributos
 	public String name;
 	public double price;
 	public int quantity;
 	
+	public Product (String name, double price, int quantity ) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	// metodos
 	public double totalvalueinstock() {
 		return price * quantity;
 	}
