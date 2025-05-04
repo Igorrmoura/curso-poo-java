f93e0bb57d5b0e5ee30279f997b63414bf7afa9c
igor <igorkfz31@gmail.com>
2025-05-04 20:53:50 -0300

    entrada de dados usando mais de um vetor

--- Vetores/src/products/Productss.java
+++ Vetores/src/products/Productss.java
@@ -10,37 +10,50 @@ public class Productss {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
-		
-		System.out.print("quantos numeros vocÊ vai digitar:  ");
-		
-		int n = sc.nextInt();
-		double soma, media; 
-		
-		double[] vect = new double[n];
-		
-		for (int i = 0; i<vect.length; i++) {
-			System.out.print("digite um numero: ");
-			vect[i] = sc.nextDouble();
-		}
-		
-		soma = 0;
-		for (int i = 0; i<vect.length; i++) {
-			soma = soma + vect[i];
-		}
-		
-		media = soma / n;
-		
-		System.out.print("VALORES = ");
-		
-		for (int i = 0; i<vect.length; i++) {
-			System.out.printf("%.1f  ", vect[i]);
-		}
-		
-		System.out.printf("\nSOMA = %.2f\n", soma);
-	    System.out.printf("MEDIA = %.2f\n", media);
 
+		int n, nmenores;
+		double alturatotal, alturamedia, percentualMenores;
 
-	    sc.close();
+		System.out.print("Quantas pessoas serao digitadas? ");
+		n = sc.nextInt();
+
+		String[] nomes = new String[n];
+		int[] idades = new int[n];
+		double[] alturas = new double[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.printf("Dados da %da pessoa:\n", i + 1);
+			System.out.print("Nome: ");
+			nomes[i] = sc.next();
+			System.out.print("Idade: ");
+			idades[i] = sc.nextInt();
+			System.out.print("Altura: ");
+			alturas[i] = sc.nextDouble();
+			System.out.println();
+		}
+
+		nmenores = 0;
+		alturatotal = 0;
+		for (int i = 0; i < n; i++) {
+			if (idades[i] < 16) {
+				nmenores++;
+			}
+			alturatotal = alturatotal + alturas[i];
+		}
+
+		alturamedia = alturatotal / n;
+		percentualMenores = ((double) nmenores / n) * 100.0;
+
+		System.out.printf("\nAltura media = %.2f\n", alturamedia);
+		System.out.printf("Pessoas com menos de 16 anos: %.1f%%\n", percentualMenores);
+
+		for (int i = 0; i < n; i++) {
+			if (idades[i] < 16) {
+				System.out.printf("Pessoas com menos de 16: %s\n", nomes[i]);
+			}
+		}
+
+		sc.close();
 	}
 
 }
