1a145a2e30c5455b0b956056069f49fd84aa42dc
igor <igorkfz31@gmail.com>
2025-05-12 19:41:47 -0300

    project with list

---
+++ Increase/src/salaryIncrease/Salary.java
@@ -0,0 +1,21 @@
+package salaryIncrease;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Salary {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(" How many employees will be registered? ");
+		int n = sc.nextInt();
+		
+		List<String> list = new ArrayList<>();
+		List<Integer> listid = new ArrayList<>();
+		List<Double> listdoub = new ArrayList<>();
+
+	}
+
+}
