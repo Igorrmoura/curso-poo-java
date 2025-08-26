b4ac9125875d5220ae49855ecaa55ab658d48ada
igor <igorkfz31@gmail.com>
2025-08-26 20:20:33 -0300

    calculos com data-hora

---
+++ Testbanc/src/application/Banch2.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.sql.*;
+
+public class Banch2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ testBanco/src/application/adcBanco.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.sql.*;
+
+
+public class adcBanco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
