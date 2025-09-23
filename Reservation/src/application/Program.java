2186ae6f501ce01795b3788ab069e29aa2fe28dd
igor <igorkfz31@gmail.com>
2025-09-23 11:34:35 -0300

    using exceptions

--- Reservation/src/application/Program.java
+++ Reservation/src/application/Program.java
@@ -13,20 +13,31 @@ public class Program {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("(dd/MM/yyyy)");
 		Scanner sc = new Scanner(System.in);
+		 sdf.setLenient(false);
 		
 		
 		System.out.print("Room Number: ");
 		int number = sc.nextInt();
 		System.out.print("Check-in date (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(sc.next());
+		Date checkin = sdf.parse(sc.next());
 		System.out.print("Check-out date (dd/MM/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
 		
-		if(! checkOut.after(checkIn)) {
+		if(! checkOut.after(checkin)) {
 			System.out.println("Error in reservation: Check-out date must after check-in date");
 		}
 		else {
-			Caveat reserv = new Caveat(number, checkIn, checkOut);
+			Caveat reserv = new Caveat(number, checkin, checkOut);
+			System.out.print("reservation: " + reserv);
+			
+			System.out.println();
+			System.out.println("Enter data to update the reservation");
+			System.out.print("check-in data (dd/MM/yyyy): ");
+			checkin = sdf.parse(sc.next());
+			System.out.print("check-out data (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			reserv.updateDates(checkin, checkOut);
 			System.out.print("reservation: " + reserv);
 		}
 
